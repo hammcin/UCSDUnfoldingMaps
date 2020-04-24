27445b6bdb44047eded7187c38529ada8553cf8f
Hamadi McIntosh <hamadi.mc@sbcglobal.net>
2020-04-24 18:28:52 -0400

    feat: Select first marker found under cursor

--- src/EarthquakeCityMap.java
+++ src/EarthquakeCityMap.java
@@ -150,7 +150,12 @@ public class EarthquakeCityMap extends PApplet {
 	// 
 	private void selectMarkerIfHover(List<Marker> markers)
 	{
-		// TODO: Implement this method
+		for (Marker marker : markers) {
+			if (marker.isInside(map, mouseX, mouseY) && lastSelected == null) {
+				lastSelected = (CommonMarker) marker;
+				marker.setSelected(true);
+			}
+		}
 	}
 	
 	/** The event handler for mouse clicks
